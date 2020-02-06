3db5d98ed44f59e1a0f3de9aafddb7a81404ed62
Kevin Caudron <kevin.caudron@103f14.iut-lens.univ-artois.fr>
2020-02-06 12:53:05 +0100

    Cliquez ici pour jouer

---
+++ app/src/main/java/fr/iutlens/mmi/invader/StartActivity.java
@@ -0,0 +1,28 @@
+package fr.iutlens.mmi.invader;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+        ImageButton btn_start = findViewById(R.id.btn_start);
+        btn_start.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(StartActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+
+        });
+    }
+}
